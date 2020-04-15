b5bb05752d5c9cd57434852fccb1b450a76d1d95
lamine messaci <lamine.messaci18@gmail.com>
2020-04-15 11:27:55 +0200

    Adding Interfaces CRUD in package database/dao "ProjectDao" and "TaskDao"

---
+++ app/src/main/java/com/cleanup/todoc/database/dao/TaskDao.java
@@ -0,0 +1,28 @@
+package com.cleanup.todoc.database.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
+
+/**
+ * Created by Lamine MESSACI on 15/04/2020.
+ */
+@Dao
+public interface TaskDao {
+    @Query("SELECT * FROM Task")
+    LiveData<List<Task>> getTasks();
+
+    @Query("SELECT * FROM Task WHERE id = :id")
+    LiveData<Task> getTask(long id);
+
+    @Insert
+    long insertTask(Task task);
+
+    @Query("DELETE FROM Task WHERE id = :id")
+    int deleteTask(long id);
+}
