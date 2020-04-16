9c1f8ad7df419e947e5b450da13bc86a89b4c8b5
lamine messaci <lamine.messaci18@gmail.com>
2020-04-16 22:48:35 +0200

    Test of ProjectDao and TaskDao with  Google class LiveDataTestUtil

---
+++ app/src/androidTest/java/com/cleanup/todoc/database/dao/ProjectDaoTest.java
@@ -0,0 +1,60 @@
+package com.cleanup.todoc.database.dao;
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import android.arch.persistence.room.Room;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.cleanup.todoc.database.CleanUpDatabase;
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.utils.LiveDataTestUtil;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lamine MESSACI on 16/04/2020.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class ProjectDaoTest {
+
+    // FOR DATA
+    private CleanUpDatabase database;
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    @Before
+    public void initDb() throws Exception {
+        this.database = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(),
+                CleanUpDatabase.class)
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    @After
+    public void closeDb() throws Exception {
+        database.close();
+    }
+
+    // DATA SET FOR TEST
+    private static long PROJECT_ID = 1;
+    private static Project PROJECT_DEMO = new Project(PROJECT_ID, "test", 0xFFEADAD1);
+
+    @Test
+    public void insertAndGetProject() throws InterruptedException {
+        // BEFORE : Adding a new project
+        this.database.projectDao().createProject(PROJECT_DEMO);
+        // TEST
+        Project project = LiveDataTestUtil.getValue(this.database.projectDao().getProject(PROJECT_ID));
+        assertTrue(project.getName().equals(PROJECT_DEMO.getName())
+                && project.getId() == PROJECT_ID
+                && project.getColor() == PROJECT_DEMO.getColor());
+    }
+}
