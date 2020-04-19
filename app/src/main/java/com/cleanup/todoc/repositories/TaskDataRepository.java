c6a03961c0677a8f0c6be318889acbd17f57f009
lamine messaci <lamine.messaci18@gmail.com>
2020-04-19 21:29:52 +0200

    Add repositories package (ProjectDataRepository && TaskDataRepository)

---
+++ app/src/main/java/com/cleanup/todoc/repositories/TaskDataRepository.java
@@ -0,0 +1,29 @@
+package com.cleanup.todoc.repositories;
+
+import android.arch.lifecycle.LiveData;
+
+import com.cleanup.todoc.database.dao.TaskDao;
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
+
+/**
+ * Created by Lamine MESSACI on 19/04/2020.
+ */
+public class TaskDataRepository {
+
+    private final TaskDao taskDao;
+
+    public TaskDataRepository(TaskDao taskDao) {
+        this.taskDao = taskDao;
+    }
+
+    // --- GET ALL TASKS ---
+    public LiveData<List<Task>> getTasks(){ return this.taskDao.getTasks(); }
+
+    // --- CREATE TASK ---
+    public void createTask(Task task){ taskDao.insertTask(task); }
+
+    // --- DELETE TASK ---
+    public void deleteTask(long taskId){ taskDao.deleteTask(taskId); }
+}
