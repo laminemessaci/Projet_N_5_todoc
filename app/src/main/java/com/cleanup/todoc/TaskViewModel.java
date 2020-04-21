31bf37d8b4879a092d6c7bf0d4eb6b9d043e1513
lamine messaci <lamine.messaci18@gmail.com>
2020-04-21 19:15:14 +0200

    Add TaskViewModel

---
+++ app/src/main/java/com/cleanup/todoc/TaskViewModel.java
@@ -0,0 +1,71 @@
+package com.cleanup.todoc;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
+import com.cleanup.todoc.repositories.ProjectDataRepository;
+import com.cleanup.todoc.repositories.TaskDataRepository;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+/**
+ * Created by Lamine MESSACI on 21/04/2020.
+ */
+public class TaskViewModel extends ViewModel {
+
+    // REPOSITORIES
+    private final TaskDataRepository taskDataSource;
+    private final ProjectDataRepository projectDataSource;
+    // EXECUTOR
+    private final Executor executor;
+    // PROJECTS
+    private LiveData<List<Project>> projects;
+
+    public TaskViewModel(TaskDataRepository taskDataSource, ProjectDataRepository projectDataSource, Executor executor) {
+        this.taskDataSource = taskDataSource;
+        this.projectDataSource = projectDataSource;
+        this.executor = executor;
+    }
+
+    // -------------
+    // FOR PROJECT
+    // -------------
+    public LiveData<List<Project>> getProjects() {
+        return projects;
+    }
+
+    // -------------
+    // FOR TASK
+    // -------------
+    public LiveData<List<Task>> getTasks() {
+        return taskDataSource.getTasks();
+    }
+
+    public void createTask(final Task task) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                taskDataSource.createTask(task);
+            }
+        });
+    }
+
+    public void deleteTask(final long taskId) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                taskDataSource.deleteTask(taskId);
+            }
+        });
+    }
+
+    public void init() {
+        if (projects == null) {
+            projects = projectDataSource.getProjects();
+        }
+    }
+
+}
